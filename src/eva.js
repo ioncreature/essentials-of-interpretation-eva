7eac0323774c19d1133740059a00f97bb0050274
ioncreature <it.is.amarenin@gmail.com>
2020-03-23 16:33:52 +0400

    added '+' operator evaluation

--- src/eva.js
+++ src/eva.js
@@ -10,6 +10,12 @@ class Eva {
         if (isString(input))
             return input.slice(1, -1);
 
+        if (Array.isArray(input)) {
+            if (input[0] === '+') {
+                return this.eval(input[1]) + this.eval(input[2]);
+            }
+        }
+
         throw 'Not Implemented';
     }
 }
--- test/index.js
+++ test/index.js
@@ -4,5 +4,7 @@ const eva = new Eva();
 
 assert(eva.eval(1), 1);
 assert(eva.eval('"hello world"'), 'hello world');
+assert(eva.eval(['+', 1, 3]), 4);
+assert(eva.eval(['+', ['+', 2, 3], 5]), 10);
 
 console.log('All assertions passed');
